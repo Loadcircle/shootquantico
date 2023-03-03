3a098e0112f21ee7e879ba8d2c463451a241b166
Jesus Milano <jesusmilano96@gmail.com>
2023-03-03 10:04:12 -0500

    404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php include("template_base/header.php"); ?>
+    <main>
+      <div class="colab">
+        <div class="colab--top">
+          <p class="title--72">404</p>
+          <p class="title--36 title--36-bold colab-p enContent">
+          Page not found
+          <br><br>
+          The page may no longer exist or may have moved to another web address. 
+          </p>
+          <p class="title--36 title--36-bold colab-p esContent">
+            Página no encontrada
+            <br><br>
+            Es posible que la página ya no exista o que se haya movido a otra dirección web.
+          </p>
+        </div>
+      </div>
+    </main>
+<?php include("template_base/footer.php"); ?>
